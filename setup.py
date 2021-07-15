9f877eccc13f97d74bd280533f6eb16f9ef61e60
Jared Neumann <janeuman@iu.edu>
2021-07-15 16:46:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['phonolex'],
   package_dir = {'phonolex': 'src/phonolex'},
   package_data={'phonolex': ['data/cmu.json', 'data/features.json', 'data/commonwords.txt', 'data/commonlemma.txt']},
-  version = '0.0.1-alpha',  license='wtfpl',
+  version = '0.0.1-alpha',  license='gplv3',
   description = 'This is a small package to describe the phonology of words and find matches for word features and/or phonological patterns.',
   author = 'Jared Neumann',
   author_email = 'janeuman@iu.edu',
@@ -14,7 +14,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: WTF Public License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3'
   ]
 )
